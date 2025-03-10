538fb0f9bcdc26faae0d1ce03f0327efad1dc9ee
quarkusio <no-reply@quarkus.io>
2025-03-10 18:52:09 +0000

    Initial commit

---
+++ src/test/java/me/cbhud/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package me.cbhud;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
